3e1bc04fbb92686c69c157389f253f06ad074349
Sok4h <55671660+Sok4h@users.noreply.github.com>
2020-11-18 18:38:47 -0500

    Mostrar contactos + borrar

---
+++ js/Contact.js
@@ -0,0 +1,30 @@
+class Contact{
+
+    constructor(contact){
+
+        this.contact=contact;
+    }
+
+    Render=()=>{
+
+        let container = document.createElement("div");
+        container.className="containerContact"
+        let name = document.createElement("p")
+        name.innerHTML=this.contact.name
+        let phone = document.createElement("p")
+        phone.innerHTML=this.contact.phone
+        let btnDelete=document.createElement("button")
+        btnDelete.innerHTML="Delete"
+        container.appendChild(name);
+        container.appendChild(phone);
+        container.appendChild(btnDelete);
+        btnDelete.addEventListener("click",()=>{
+
+            let database = firebase.database();
+            database.ref("Contacts/"+this.contact.id).set(null);
+        })
+
+        return container;
+
+    }
+}
--- js/home.js
+++ js/home.js
@@ -2,8 +2,10 @@ const database = firebase.database();
 const auth = firebase.auth();
 const title = document.getElementById("title");
 const btnNewContact = document.getElementById("btnNewContact")
-const inputContactName = document.getElementById("inputPhone")
+const inputContactName = document.getElementById("inputName")
 const inputContactPhone = document.getElementById("inputPhone")
+const containerContacts = document.getElementById("containerContacts")
+
 let activeUser;
 
 auth.onAuthStateChanged(
@@ -17,9 +19,11 @@ auth.onAuthStateChanged(
             database.ref("Users/" + user.uid).once("value", (data) => {
 
                 let userdb = data.val();
-                activeUser=userdb;
+                activeUser = userdb;
 
                 title.innerHTML = userdb.name;
+
+                LoadContacts()
 
             })
         }
@@ -31,25 +35,50 @@ auth.onAuthStateChanged(
         }
     }
 )
+
+LoadContacts = () => {
+
+    database.ref("Contacts").orderByChild("userId").equalTo(activeUser.id).on("value",(data)=>{
+
+        containerContacts.innerHTML=""
+
+        data.forEach(
+            
+            contact=>{
+
+                let info= contact.val();
+                let tempContact = new Contact(info)
+                containerContacts.appendChild(tempContact.Render())
+
+            }
+        )
+
+    })
+
+
+}
 AddContact = () => {
 
     //verifica si algun campo está vacio
-    if(inputContactPhone.value==""||inputContactName.value==""){
+    if (inputContactPhone.value == "" || inputContactName.value == "") {
 
         alert("Please fill all blank fields");
 
     }
-    else{
+    else {
 
         let reference = database.ref("Contacts").push();
 
-        let contact={
+        let contact = {
 
-        id:reference.key,
-        userId:activeUser.id,
-        name:inputContactName.value,
-        phone:inputContactPhone.value
+            id: reference.key,
+            userId: activeUser.id,
+            name: inputContactName.value,
+            phone: inputContactPhone.value
         }
+
+        inputContactName.val=""
+        inputContactPhone.val=""
         reference.set(contact);
 
     }
